f4d44f0f39685399dfa9a80c4bbe88e45af2f862
cesarfuster <cfuster@uol.com.br>
2018-06-17 10:23:14 -0300

    Update controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  resources :spendings
+  # /spendings_export
+  get '/spending_export' => 'spendings#export'
+  resources :agreements do
+    member do
+      # /agreements/:id/export
+      get 'export'
+    end
+  end
 end
